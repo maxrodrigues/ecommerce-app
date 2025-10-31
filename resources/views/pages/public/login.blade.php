dfccfb8f93f98c7bc21fd7f2f0924c4d9f189e2a
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-10-31 17:17:16 -0300

    feat: add admin login link

--- resources/views/pages/public/login.blade.php
+++ resources/views/pages/public/login.blade.php
@@ -8,43 +8,48 @@
                     <span class="text-sm text-lime-700">Login in to your FreshMart account</span>
                 </div>
             </div>
-            <div class="w-full space-y-3">
-                <div class="flex flex-col space-y-1.5">
-                    <label class="text-sm">E-mail or Username</label>
-                    <input
-                        type="text"
-                        class="bg-secondary placeholder:text-lime-700 px-3 py-2 rounded-md outline-none"
-                        placeholder="Enter your email or username"
-                    />
-                </div>
-                <div class="flex flex-col space-y-1.5">
-                    <label class="text-sm">Password</label>
-                    <div class="relative flex items-center">
+            <form class="">
+                <div class="w-full space-y-3">
+                    <div class="flex flex-col space-y-1.5">
+                        <label class="text-sm">E-mail or Username</label>
                         <input
-                            type="password"
-                            class="bg-secondary placeholder:text-lime-700 px-3 py-2 rounded-md outline-none w-full"
-                            placeholder="Enter your password"
+                            type="text"
+                            class="bg-secondary placeholder:text-lime-700 px-3 py-2 rounded-md outline-none"
+                            placeholder="Enter your email or username"
                         />
-                        <i class="fa-solid fa-eye-slash absolute right-3 text-primary"></i>
                     </div>
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="flex flex-row space-x-1">
-                        <input type="checkbox" id="remember"/>
-                        <div>
-                            <label for="remember" class="text-sm text-lime-700">Remeber me</label>
+                    <div class="flex flex-col space-y-1.5">
+                        <label class="text-sm">Password</label>
+                        <div class="relative flex items-center">
+                            <input
+                                type="password"
+                                class="bg-secondary placeholder:text-lime-700 px-3 py-2 rounded-md outline-none w-full"
+                                placeholder="Enter your password"
+                            />
+                            <i class="fa-solid fa-eye-slash absolute right-3 text-primary"></i>
                         </div>
                     </div>
-                    <span class="text-primary font-bold text-sm">Forgot Password?</span>
+                    <div class="flex items-center justify-between mt-10">
+                        <div class="flex flex-row space-x-1">
+                            <input type="checkbox" id="remember"/>
+                            <div>
+                                <label for="remember" class="text-sm text-lime-700">Remeber me</label>
+                            </div>
+                        </div>
+                        <span class="text-primary font-bold text-sm">Forgot Password?</span>
+                    </div>
+                    <div>
+                        <button class="bg-primary w-full rounded-md text-white font-semibold py-2 text-center">Login</button>
+                    </div>
                 </div>
-                <div>
-                    <button class="bg-primary w-full rounded-md text-white font-semibold py-2 text-center">Login</button>
-                </div>
-            </div>
+            </form>
         </div>
-        <div class="pb-6">
+        <div class="pb-6 flex flex-col items-center">
             <span class="text-lime-700">Don't have an account?
                 <a href="#" class="text-primary font-semibold">Create an Account</a>
+            </span>
+            <span class="text-lime-900 text-xs pt-1">
+                <a href="{{ route('admin.login') }}" class="font-semibold">Access admin panel</a>
             </span>
         </div>
     </div>
